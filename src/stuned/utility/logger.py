215516e57d5b9cbfbeb9fc7f9908a8560b8a4054
Alexander Rubinstein <rubalex14@gmail.com>
2023-08-30 20:18:11 +0200

    [AADJL] Prepare for filling table: - Allow multiple wandb logging on the same step (tested by 1).
    Tests:
    - 1: [AADJL.T] Stable step for wandb.

--- src/stuned/utility/logger.py
+++ src/stuned/utility/logger.py
@@ -747,7 +747,7 @@ def try_to_sync_csv_with_remote(logger, sync_row_zero=True):
 def try_to_log_in_wandb(logger, dict_to_log, step):
     wandb_run = logger.wandb_run
     if wandb_run is not None:
-        if step <= wandb_run.step:
+        if step < wandb_run.step:
             step = wandb_run.step + 1
         wandb_run.log(dict_to_log, step=step)
 
