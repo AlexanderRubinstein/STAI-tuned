ac77d22764f034ae525e51f7b0a3c3307a0bb5f1
AlexanderRubinstein <rubalex14@gmail.com>
2024-07-30 21:03:42 +0200

    Fix run_cmd_through_popen

--- src/stuned/run_cmd/runner.py
+++ src/stuned/run_cmd/runner.py
@@ -8,18 +8,10 @@ import threading
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from utility.utils import (
     NEW_SHELL_INIT_COMMAND,
-    # raise_unknown,
-    # check_dict,
     get_with_assert,
-    # update_dict_by_nested_key,
-    # pretty_json,
-    # runcmd,
     log_or_print,
     error_or_print
 )
-# from utility.logger import (
-#     log_or_print
-# )
 sys.path.pop(0)
 
 
@@ -47,7 +39,6 @@ def runner(
 
     logger.log(f"Running command:\n{cmd_to_run}", auto_newline=True)
     logger.log(f"As one line:\n{cmd_to_run}", auto_newline=False)
-    # runcmd(cmd_to_run, logger=logger, verbose=False)
     run_cmd_through_popen(cmd_to_run, logger)
 
 
@@ -67,7 +58,6 @@ def make_task_cmd(exec_path, kwargs, is_python, conda_env, logger):
             NEW_SHELL_INIT_COMMAND,
             conda_env
         )
-        conda_cmd = conda_cmd.replace("source", "sh")
     else:
         conda_cmd = ""
 
@@ -76,122 +66,64 @@ def make_task_cmd(exec_path, kwargs, is_python, conda_env, logger):
     else:
         python_cmd = ""
 
-    # output_str = f" >> {logger.stdout_file} 2>>{logger.stderr_file}"
-
     return (
         conda_cmd
         + python_cmd
         + exec_path
         + kwargs_str
-        # + output_str
     )
 
 
 def run_cmd_through_popen(cmd_to_run, logger):
 
-    # def read_stdout(process):
-    #     for stdout_line in iter(process.stdout.readline, ''):
-    #         print(f"STDOUT: {stdout_line}", end='')  # Process the line from stdout
+    def read_out(process, out_type, logger):
+        buffer = ""
+        if out_type == "stdout":
+            out = process.stdout
+            log_func = log_or_print
+        else:
+            assert out_type == "stderr"
+            out = process.stderr
+            log_func = error_or_print
 
-    # def read_stderr(process):
-    #     for stderr_line in iter(process.stderr.readline, ''):
-    #         print(f"STDERR: {stderr_line}", end='')  # Process the line from stderr
+        while True:
+            output = out.readline()
+            if output == '' and process.poll() is not None:
+                break
 
-    # def read_out(output, log_type):
-    def read_out(output, log_func, logger):
-        print("test read_out")
-        # for stdout_line in iter(output.readline, ''):
-        # while True:
-            # for stdout_line in iter(lambda: output.read(1), b""):
-            #     print("test")
-            #     # if log_type == "error":
-            #     #     # error_or_print(stdout_line, logger)
-            #     #     # print(stdout_line, file=sys.stderr)
-            #     #     # out = sys.stderr
-            #     #     log_func = error_or_print
-            #     # else:
-            #     #     assert log_type == "info"
-            #     #     # out = sys.stdout
-            #     #     log_func = log_or_print
-            #         # log_or_print(stdout_line, logger)
-            #     # print(stdout_line, file=out)
-            #     log_func(stdout_line, logger)
-        for stdout_line in iter(lambda: output.read(1), b""):
-            print("test")
-            log_func(stdout_line, logger)
+            buffer += output
+            if output == '\n':
+                log_func(buffer, logger)
+                buffer = ""
 
-    # Start the process
-    # process = subprocess.Popen(
-    #     # ['your_command', 'arg1', 'arg2'],??
-    #     cmd_to_run.split(),
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    #     # text=True,
-    #     shell=True
-    # )
-    cmd_as_list = cmd_to_run.split()
-    cmd_as_list = [
-        "echo",
-        "check test 1",
-        "&&",
-        '/usr/bin/sh',
-        '/home/oh/arubinstein17/.bashrc',
-        "&&",
-        "echo",
-        "check test",
-        # '&&',
-        # 'conda',
-        # 'activate',
-        # './envs/seg_env',
-        # '&&',
-        # 'python',
-        # '/mnt/lustre/work/oh/arubinstein17/github/densification/src/densifier/proposal_generation.py',
-        # '--config-file',
-        # './src/densifier/get_segments/cropformer_hornet.yaml',
-        # '--input',
-        # './data/CounterAnimal/symlinked/common.tar.gz',
-        # '--opts',
-        # 'MODEL.WEIGHTS',
-        # './models/CropFormer_hornet_3x/CropFormer_model/Entity_Segmentation/CropFormer_hornet_3x/CropFormer_hornet_3x_03823a.pth',
-        # '--output',
-        # './data/masks/common_masks_2907.pkl'
-    ]
+        if buffer != "":
+            log_func(buffer, logger)
+
     process = subprocess.Popen(
-        # ['your_command', 'arg1', 'arg2'],??
-        cmd_as_list,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-        # text=True,
-        # shell=True
+        cmd_to_run,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
     )
-    print(cmd_to_run.split()) # tmp
 
     log_or_print(f"Process started by runner has id: {process.pid}", logger)
 
-    # # Start threads to read stdout and stderr
-    # stdout_thread = threading.Thread(
-    #     target=read_out,
-    #     args=(process.stdout,log_or_print,logger)
-    # )
-    # stderr_thread = threading.Thread(
-    #     target=read_out,
-    #     args=(process.stderr,error_or_print,logger)
-    # )
+    stdout_thread = threading.Thread(
+        target=read_out,
+        args=(process,"stdout",logger)
+    )
+    stderr_thread = threading.Thread(
+        target=read_out,
+        args=(process,"stderr",logger)
+    )
 
-    # stdout_thread.start()
-    # stderr_thread.start()
+    stdout_thread.start()
+    stderr_thread.start()
 
-    # # sys.stderr = process.stderr
-    # # sys.stdout = process.stdout # tmp
+    stdout_thread.join()
+    stderr_thread.join()
 
-    # # # Wait for the process to complete
-    # # process.wait()
-
-    # # Wait for the threads to finish
-    # stdout_thread.join()
-    # stderr_thread.join()
-
-    # Wait for the process to complete
     process.wait()
 
     if process.returncode != 0:
@@ -199,3 +131,4 @@ def run_cmd_through_popen(cmd_to_run, logger):
             f"Process failed with return code: {process.returncode}. "
             f"Check stderr for details"
         )
+
