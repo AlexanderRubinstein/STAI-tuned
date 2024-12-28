f75081202e6dc11e1cace4731a92fa139e84b5ab
AlexanderRubinstein <rubalex14@gmail.com>
2024-12-28 16:51:26 +0100

    Allow for bash, single and two dash flags in runner;
    Battle test: https://www.notion.so/STUNED-T-Runnign-with-Flags-and-bash-16ae34b2741880c79540c5efeba0c8a7?pvs=4

--- src/stuned/run_cmd/runner.py
+++ src/stuned/run_cmd/runner.py
@@ -36,7 +36,10 @@ def runner(
     cmd_to_run = make_task_cmd(
         get_with_assert(experiment_config, "exec_path"),
         experiment_config.get("kwargs"),
+        experiment_config.get("two_dash_flags"),
+        experiment_config.get("single_dash_flags"),
         experiment_config.get("is_python", True),
+        experiment_config.get("is_bash", False),
         get_with_assert(experiment_config, "conda_env"),
         logger=logger
     )
@@ -46,18 +49,43 @@ def runner(
     run_cmd_through_popen(cmd_to_run, logger)
 
 
-def make_task_cmd(exec_path, kwargs, is_python, conda_env, logger):
+def make_task_cmd(
+    exec_path,
+    kwargs,
+    two_dash_flags,
+    single_dash_flags,
+    is_python,
+    is_bash,
+    conda_env,
+    logger
+):
 
     if kwargs is not None:
         kwargs_str = " " + " ".join([
-            "--{} {}".format(k, v)
+            "--{}={}".format(k, v)
             for k, v in kwargs.items()
         ])
     else:
         kwargs_str = ""
 
+    if two_dash_flags is not None:
+        two_dash_flags_str = " " + " ".join([
+            "--{}".format(arg)
+            for arg in two_dash_flags
+        ])
+    else:
+        two_dash_flags_str = ""
+
+    if single_dash_flags is not None:
+        single_dash_flags_str = " " + " ".join([
+            "-{}".format(arg)
+            for arg in single_dash_flags
+        ])
+    else:
+        single_dash_flags_str = ""
+
     if conda_env is not None:
-        assert is_python
+        # assert is_python
         conda_cmd = "{} {} && ".format(
             NEW_SHELL_INIT_COMMAND,
             conda_env
@@ -70,71 +98,17 @@ def make_task_cmd(exec_path, kwargs, is_python, conda_env, logger):
     else:
         python_cmd = ""
 
+    if is_bash:
+        bash_cmd = "bash "
+    else:
+        bash_cmd = ""
+
     return (
         conda_cmd
         + python_cmd
+        + bash_cmd
         + exec_path
         + kwargs_str
+        + two_dash_flags_str
+        + single_dash_flags_str
     )
-
-
-# def run_cmd_through_popen(cmd_to_run, logger):
-
-#     def read_out(process, out_type, logger):
-#         buffer = ""
-#         if out_type == "stdout":
-#             out = process.stdout
-#             log_func = log_or_print
-#         else:
-#             assert out_type == "stderr"
-#             out = process.stderr
-#             log_func = error_or_print
-
-#         while True:
-#             output = out.readline()
-#             if output == '' and process.poll() is not None:
-#                 break
-
-#             buffer += output
-
-#             if output == '\n' or len(buffer) > MAX_BUFFER_SIZE:
-
-#                 log_func(buffer, logger)
-#                 buffer = ""
-
-#         if buffer != "":
-#             log_func(buffer, logger)
-
-#     process = subprocess.Popen(
-#         cmd_to_run,
-#         shell=True,
-#         stdout=subprocess.PIPE,
-#         stderr=subprocess.PIPE,
-#         text=True
-#     )
-
-#     log_or_print(f"Process started by runner has id: {process.pid}", logger)
-
-#     stdout_thread = threading.Thread(
-#         target=read_out,
-#         args=(process,"stdout",logger)
-#     )
-#     stderr_thread = threading.Thread(
-#         target=read_out,
-#         args=(process,"stderr",logger)
-#     )
-
-#     stdout_thread.start()
-#     stderr_thread.start()
-
-#     stdout_thread.join()
-#     stderr_thread.join()
-
-#     process.wait()
-
-#     if process.returncode != 0:
-#         raise Exception(
-#             f"Process failed with return code: {process.returncode}. "
-#             f"Check stderr for details"
-#         )
-
